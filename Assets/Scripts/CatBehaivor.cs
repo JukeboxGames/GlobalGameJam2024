75e5559fd08c311e00394c4d6547eb866df35b71
PabloGP88 <pablogpp88@gmail.com>
2024-01-27 16:49:06 -0600

    Toma

---
+++ Assets/Scripts/CatBehaivor.cs
@@ -0,0 +1,47 @@
+using System.Collections;
+using System.Collections.Generic;
+using UnityEngine;
+
+public class CatBehaivor : MonoBehaviour
+{
+    [SerializeField] private Animator animator;
+    [SerializeField] private SpriteRenderer catHead;
+    [SerializeField] private float animationTime = 0.5f;
+    private bool isHitting = false;
+    private void Update() 
+    {
+      if (Input.GetMouseButtonDown(0))
+      {
+        if (!isHitting)
+        {
+            StartCoroutine(HittingAnimation());
+        }
+      }  
+    }
+
+    IEnumerator HittingAnimation()
+    {
+        isHitting = true;
+
+        if (Camera.main.ScreenToWorldPoint(Input.mousePosition).x > 0)
+        {
+            animator.SetBool("Hit",true);
+        } 
+        else if (Camera.main.ScreenToWorldPoint(Input.mousePosition).x < 0)
+        {
+            animator.SetBool("Hit",true);
+            catHead.flipX = true;
+        } 
+        else 
+        {
+            animator.SetBool("hitLeft",true);
+            catHead.flipX = true;
+        }
+
+        yield return new WaitForSeconds(animationTime);
+
+        isHitting = false;
+        catHead.flipX = false;
+        animator.SetBool("Hit",false);
+    }
+}
