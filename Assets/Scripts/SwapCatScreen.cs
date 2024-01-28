5b1a40c12c7c4acc27c313823c79b8f56ea83a6f
josecarl0s1 <72897182+josecarl0s1@users.noreply.github.com>
2024-01-28 11:39:56 -0600

    CatSwap

---
+++ Assets/Scripts/SwapCatScreen.cs
@@ -0,0 +1,37 @@
+using System.Collections;
+using System.Collections.Generic;
+using UnityEngine;
+using System; 
+using UnityEngine.UI;
+
+public class SwapCatScreen : MonoBehaviour
+{
+    public Sprite[] catImages;
+    public ScoringSystem scorer;
+    private float score; 
+    public Image rend; 
+    private int index; 
+     static int MapNumberToOutput(int x)
+    {
+        // Define the range size
+        int rangeSize = 20;
+
+        // Calculate the index of the range
+        int rangeIndex = (int)Math.Ceiling((double)x / rangeSize);
+
+        // Cap the range index to 5
+        rangeIndex = Math.Min(rangeIndex, 5);
+
+        return rangeIndex;
+    }
+    void Update(){
+        score = scorer.GetScore();
+        index = MapNumberToOutput((int)score);
+        Debug.Log(index);
+        Console.WriteLine(index);
+        if(index != 0){
+            rend.sprite = catImages[index-1];
+        }
+    }
+    
+}
