b3fe67138bc0046c29b8d1d88c0c4d7560070861
josecarl0s1 <72897182+josecarl0s1@users.noreply.github.com>
2024-01-28 03:03:51 -0600

    Better Hair

--- Assets/Scripts/HairController.cs
+++ Assets/Scripts/HairController.cs
@@ -9,17 +9,26 @@ public class HairController : MonoBehaviour
     public Sprite[] hairTypes; 
     private int randomHairColor; 
     private int randomHairType;  
-    
+    private int bald; 
+    private bool didItHappen = false; 
     // Start is called before the first frame update
     void Awake()
     {
         hair = GameObject.FindGameObjectsWithTag("Hair");
         randomHairColor = Random.Range(0, hairColors.Length - 1);
         randomHairType = Random.Range(0, hairTypes.Length -1);
-        for(int i = 0; i < hair.Length; i++){
+        bald = Random.Range(0, 100);
+        if(bald == 89){
+            foreach (GameObject strand in hair){
+                Destroy(strand);
+            }
+        }
+
+        for(int i = 0; i < hair.Length && !didItHappen; i++){
             hair[i].GetComponent<SpriteRenderer>().color = hairColors[randomHairColor];
             hair[i].GetComponent<SpriteRenderer>().sprite = hairTypes[randomHairType];
         }
+        
     }
 
 }
