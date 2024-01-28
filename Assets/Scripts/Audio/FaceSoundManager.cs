0a9e4803066db90cd49b07e76bac5f62594feeaf
PabloGP88 <pablogpp88@gmail.com>
2024-01-28 12:42:49 -0600

    new mouths

--- Assets/Scripts/Audio/FaceSoundManager.cs
+++ Assets/Scripts/Audio/FaceSoundManager.cs
@@ -7,10 +7,43 @@ public class FaceSoundManager : MonoBehaviour
 {
     [SerializeField] SoundEffectManager soundEffects;
     [SerializeField] Face face;
+    [SerializeField] private GameObject bloodEffect;
+    [SerializeField] private List<Transform> faceParts;
     private void OnEnable() 
     {
        face.HitPlayerEvent     += HandlePlayerHit;
        face.NotHitPlayerEvent  += HandleNotPlayerHit;
+    }
+
+    private void Update() 
+    {
+        if (Input.GetMouseButtonDown(0))
+        {
+            CheckAllChilds();
+        } 
+    }
+
+    private void CheckAllChilds()
+    {
+        foreach(Transform transform in faceParts)
+        {
+            if (transform.GetChild(0).TryGetComponent<ChildFeature>(out ChildFeature child))
+            {
+                if(child.OverMe) 
+                { 
+                    HandlePlayerHit(); 
+
+                    Vector3 mousePosition = new(
+                        Camera.main.ScreenToWorldPoint(Input.mousePosition).x,
+                        Camera.main.ScreenToWorldPoint(Input.mousePosition).y,
+                        0
+                    );
+
+                    bloodEffect.transform.position = mousePosition;
+                    bloodEffect.GetComponent<ParticleSystem>().Play();
+                }
+            }
+        }
     }
     private void HandlePlayerHit()
     {
